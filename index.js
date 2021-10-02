b135e427b95eb1be43ba9175772e5ec7bea89ec0
Mutlaq Aldhbuiub <matulkn@gmail.com>
2021-10-02 23:28:22 +0300

    update index.js :tada:

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
+const CHANNEL_NAME = "Mutlaq_777"
+
 const tmi = require('tmi.js');
 const OBSWebSocket = require('obs-websocket-js');
+const obs = new OBSWebSocket();
 
 const client = new tmi.Client({
-	channels: [ 'KsaVegas' ]
+	channels: [ CHANNEL_NAME ]
 });
-
 client.connect();
-const obs = new OBSWebSocket();
-obs.connect({ address: '192.168.100.6:4444' ,password:'password'}).then(() => {
+
+obs.connect({ address: 'localhost:4444' ,password:'password'}).then(() => {
         console.log(`Obs up`);
     }).catch(err => {
         console.log(err);
     });
-
     obs.on('error', err => {
         console.error('socket error:', err);
     });
 
 client.on('message', (channel, tags, message, self) => {
-                 if(message.includes('!chat')){
-                    obs.send('SetCurrentScene', { 'scene-name': "End"});
-                }
-
-                if(message.includes('!mohad')){
-                    obs.send('GetSceneList').then(data => {
-                        console.log('Using promises:', data);
-                    });
-                }    
+    if(message.includes('!chat')){
+        obs.send('SetCurrentScene', { 'scene-name': "End"});
+    }
+    if(message.includes('!mohad')){
+        obs.send('GetSceneList').then(data => {
+            console.log('Using promises:', data);
+        });
+    }    
 });
-					
